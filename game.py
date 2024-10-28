f6714e4c42fede2b4436bd2241b35a1fc8fa3f37
xy-wu <wu.xuanyu@hotmail.com>
2024-10-28 19:28:57 +0900

    Optimize for 1 door and single layout

--- game.py
+++ game.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from itertools import combinations, permutations
 
-from holoviews.plotting.bokeh.styles import alpha
+from itertools import combinations, permutations
+import random
+
 
 EXAMINE_DIRS8 = (
     (1, 0),
@@ -65,9 +66,10 @@ def gen_fp(xlim: int = 32, ylim: int = 32):
     fp[15, :] = 0
     return fp
 
+
 def separate_pixels(p_grid):
     blocked, walkable = [], []
-    n, m = len(p_grid), len(p_grid)
+    n, m = len(p_grid), len(p_grid[0])
     for i in range(n):
         for j in range(m):
             if p_grid[i][j].type == 0:
@@ -76,11 +78,13 @@ def separate_pixels(p_grid):
                 walkable.append(p_grid[i][j])
     return blocked, walkable
 
+
 def gen_sample_points(candidates, n_points=20):
     sp = []
     for i in np.random.choice(len(candidates), n_points):
         sp.append(candidates[i])
     return sp
+
 
 def extract_from_2d_array(arr, idx):
     ret = []
@@ -96,6 +100,7 @@ def get_candidate_dirs(current, xlim, ylim):
             candidate_dirs.append(idir)
     return candidate_dirs
 
+
 def walk_on_wall(fp, current):
     for dir in EXAMINE_DIRS8:
         continue
@@ -109,6 +114,11 @@ class Pixel:
         self.type = p_type  # 0: wall, 1: walkable, for now
         self.visited = False
         self.index = index
+
+    def swap_status(self, other):
+        self.type, other.type = other.type, self.type
+        self.visited, other.visited = other.visited, self.visited
+        self.neighbors, other.neighbors = other.neighbors, self.neighbors
 
     @staticmethod
     def from_grid(grid):
@@ -135,6 +145,36 @@ class Pixel:
                 grid[row, col] = pixel.type
         return grid
 
+
+class Agent:
+    def __init__(self, agent: Pixel = None):
+        self.agent = self.init_agent(agent) if agent else None
+        self.score = 1000
+
+    def init_agent(self, agent):
+        agent.type = 1
+        return agent
+
+    def step(self):
+        next = self.pick_from_candidates(self.agent.neighbors)
+        # self.agent.swap_status(next)
+        # self.agent.type = 0  # reset
+        # self.agent = next
+        # self.agent.type = 1
+        self.set_to(next)
+
+    def set_to(self, new_agent):
+        self.agent.type = 0  # reset
+        self.agent = new_agent
+        self.agent.type = 1
+
+    def pick_from_candidates(self, candidates):
+        random.shuffle(candidates)
+        for c in candidates:
+            if c.type != 0:
+                continue
+            return c
+        return None
 
 
 # visualization
@@ -171,6 +211,10 @@ class Visualize:
         plt.show()
 
 
+def optimize_single_step():
+    pass
+
+
 def main():
     # settings
     # np.random.seed(0)
@@ -178,23 +222,35 @@ def main():
 
     fp = gen_fp(xlim, ylim)
     Pixel.from_grid(fp)
-    pixels = Pixel.pGrid
+    p_grid = Pixel.pGrid
 
-    blocked, walkable = separate_pixels(pixels)
+    blocked, walkable = separate_pixels(p_grid)
 
     sp = gen_sample_points(walkable, 10)
 
-    grid = Pixel.to_grid()
-
     # optimize
-    # door = blocked
-    for pairs in combinations(sp, 2):
-        s = pairs[0].index
-        e = pairs[1].index
-        _, score = aStar(grid, s, e)
+    door = Agent(blocked[0])
+    print(door.agent.type)
+    best_score = 1000
+    best_pos = door.agent
+    for _ in range(100):
+        score_avg = 0
+        for pairs in combinations(sp, 2):
+            grid = Pixel.to_grid()
+            s = pairs[0].index
+            e = pairs[1].index
+            _, score = aStar(grid, s, e)
+            score_avg += score
+        score_avg /= len(sp) * (len(sp) - 1) / 2
+        if best_score > score_avg:
+            best_score = score_avg
+            best_pos = door.agent
+        door.step()
 
+    door.set_to(best_pos)
 
     # visualize
+    grid = Pixel.to_grid()
     Visualize.draw_grid(grid)
     for pairs in combinations(sp, 2):
         s = pairs[0].index
