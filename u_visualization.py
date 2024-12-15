af5190b0c4c68ca1e0d0c12c5ba377192fb18c0b
wxy <xy-wu@outlook.com>
2024-12-15 17:06:35 +0900

    Add debug messages

--- u_visualization.py
+++ u_visualization.py
@@ -36,7 +36,7 @@ class Visualizer:
                     alpha=0.3,
                 )
 
-    def draw_mesh(self, mesh: "Mesh", title=None, show=True, draw_text=False):
+    def draw_mesh(self, mesh, title=None, show=True, draw_text=""):
         for f in mesh.faces:
             if f is None:
                 continue
@@ -52,15 +52,11 @@ class Visualizer:
             if v is None:
                 continue
             plt.scatter(v.x, v.y, c="k", s=10)
-            if draw_text:
-                plt.text(
-                    v.x,
-                    v.y,
-                    str(v.guid),
-                    fontsize=12,
-                    horizontalalignment="right",
-                    verticalalignment="top",
-                )
+
+        if draw_text:
+            self.draw_infos(
+                mesh, f="f" in draw_text, e="e" in draw_text, v="v" in draw_text
+            )
 
         if title:
             self.fig.set_title(title)
@@ -68,6 +64,54 @@ class Visualizer:
 
         if show:
             plt.show()
+
+    def draw_infos(self, mesh, f=False, e=False, v=False):
+        if f:
+            for f in mesh.faces:
+                if f is None:
+                    continue
+                self.fig.text(
+                    f.x,
+                    f.y,
+                    str(f.fid),
+                    fontsize=12,
+                    horizontalalignment="right",
+                    verticalalignment="top",
+                )
+
+        if e:
+            mesh.reset_visited(mesh.edges)
+            for f in mesh.faces:
+                if f is None:
+                    continue
+                for e in f.half_edges:
+                    if e is None or e.visited:
+                        continue
+                    self.fig.text(
+                        e.mid()[0],
+                        e.mid()[1],
+                        str(f"{e.eid}/{e.twin.eid}" if e.twin else e.eid),
+                        fontsize=10,
+                        horizontalalignment="center",
+                        verticalalignment="center",
+                        c="r" if e.is_blocked else "g",
+                    )
+                    e.visited = True
+                    if e.twin:
+                        e.twin.visited = True
+
+        if v:
+            for v in mesh.vertices:
+                if v is None:
+                    continue
+                self.fig.text(
+                    v.x,
+                    v.y,
+                    str(v.vid),
+                    fontsize=12,
+                    horizontalalignment="right",
+                    verticalalignment="top",
+                )
 
     def show(self, title=None):
         self.fig.set_title(title)
